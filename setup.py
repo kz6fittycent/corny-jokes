f6b17fd96a9c4002cf89961a8f7839b1b9260b80
kz6fittycent <jimmy.tigert@gmail.com>
2018-02-13 00:47:35 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name="cornyjokes",
+    version="1.0.0",
+    description="A collection of corny, clean jokes for the terminal",
+    long_description=open("README.md").read(),
+    license="MIT",
+    packages=["libcornyjokes"],
+    scripts=["cornyjokes"],
+    package_data={"libcornyjokes": ["data/*"]},
+)
