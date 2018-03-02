3739f4f2a1607b7a2b6434b424e1183fb64eb7db
kz6fittycent <jimmy.tigert@gmail.com>
2018-03-01 22:02:40 -0600

    version 2 (prep)
    prepping for new code, adding sound effects

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="cornyjokes",
-    version="1.0.0",
+    version="2.0.0",
     description="A collection of corny, clean jokes for the terminal",
     long_description=open("README.md").read(),
     license="MIT",
