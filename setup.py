74f6bb54ef16015990adb5d1d9c27dda251ab35f
kz6fittycent <jimmy.tigert@gmail.com>
2022-03-23 11:23:02 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="cornyjokes",
-    version="3.0",
+    version="3.1",
     description="A collection of corny, clean jokes for the terminal",
     long_description=open("README.md").read(),
     license="MIT",
